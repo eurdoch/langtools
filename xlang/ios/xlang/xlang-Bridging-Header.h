f7c27166e29da3021bfdc90284072454fa2dfc4b
George Balch <gbalch714@gmail.com>
2025-07-25 21:15:45 -0700

    Remove xlang

--- xlang/ios/xlang/xlang-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  xlang-Bridging-Header.h
-//  xlang
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
